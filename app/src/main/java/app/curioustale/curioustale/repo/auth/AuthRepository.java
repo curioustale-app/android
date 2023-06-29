1c8defdac481cfab29830da1d77fcefa6f251d3b
atul <atulpatare99@gmail.com>
2023-06-29 17:10:12 +0530

    added auth repository for login/ signup

---
+++ app/src/main/java/app/curioustale/curioustale/repo/auth/AuthRepository.java
@@ -0,0 +1,12 @@
+package app.curioustale.curioustale.repo.auth;
+
+public interface AuthRepository<T> {
+    boolean isUserSignedIn();
+    void signAnonymously(AnonymousSignInListener<T> listener);
+    T getCurrentUser();
+
+    interface AnonymousSignInListener<T> {
+        void success(T user);
+        void error(Exception e);
+    }
+}
---
+++ app/src/main/java/app/curioustale/curioustale/repo/auth/FirebaseAuthRepository.java
@@ -0,0 +1,35 @@
+package app.curioustale.curioustale.repo.auth;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class FirebaseAuthRepository implements AuthRepository<FirebaseUser> {
+    private final FirebaseAuth firebaseAuth;
+
+    public FirebaseAuthRepository() {
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public boolean isUserSignedIn() {
+        return firebaseAuth.getCurrentUser() != null;
+    }
+
+    @Override
+    public void signAnonymously(AnonymousSignInListener<FirebaseUser> listener) {
+        firebaseAuth.signInAnonymously()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = firebaseAuth.getCurrentUser();
+                        listener.success(user);
+                    } else {
+                        listener.error(task.getException());
+                    }
+                });
+    }
+
+    @Override
+    public FirebaseUser getCurrentUser() {
+        return firebaseAuth.getCurrentUser();
+    }
+}
