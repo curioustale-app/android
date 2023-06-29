a3c317b7be16a6409515ff9eda3eb9d1c9ca842e
atul <atulpatare99@gmail.com>
2023-06-29 17:11:29 +0530

    integrated the repo in the splash screen

---
+++ app/src/main/java/app/curioustale/curioustale/config/Constants.java
@@ -0,0 +1,6 @@
+package app.curioustale.curioustale.config;
+
+public class Constants {
+    private Constants() {throw new IllegalStateException("Utility class");}
+    public static final String DEBUG_TAG = "curious-tale-debug";
+}
